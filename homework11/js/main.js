1d92448d8bc150fdb23bcbcb5d10ed35980f5dfa
Anastasia <68748929+fatykhava@users.noreply.github.com>
2020-12-09 17:25:42 +0300

    feat: add form submit

--- homework11/js/main.js
+++ homework11/js/main.js
@@ -2,14 +2,27 @@ const checkString = (e) => {
   e.target.value = e.target.value.trim();
 };
 
-const checkNumber = (e) => {
-  if (e.target.value < 0) {
-    e.target.value = '';
+const checkAllInput = () => {
+  const usersNumb = document.querySelector('#users-during-day');
+  const webName = document.querySelector('#web-name');
+
+  if (usersNumb.value < 0) {
+    usersNumb.value = '';
+    alert('Число посетителей не может быть отрицательным!');
+    return;
   }
+
+  if (webName.value.trim().length < 3) {
+    webName.value = '';
+    alert('Название сайта должно быть более 3 символов');
+    return;
+  }
+
+  form.submit();
 };
 
 document.querySelectorAll('[type="text"]').forEach((input) =>{
   input.addEventListener('change', checkString);
 })
 
-document.querySelector('[type="number"]').addEventListener('change', checkNumber);
+document.querySelector('.submit').addEventListener('click', checkAllInput);
